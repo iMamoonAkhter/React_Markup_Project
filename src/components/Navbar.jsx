61b655a1cede8405e025de3a867258f9ea61c919
imamoonakhter <imamoonakhter@gmail.com>
2024-11-30 13:50:25 +0500

    Markup React Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import reactlogo from "../assets/react.svg"
+const Navbar = () => {
+  return (
+    <main>
+        <img src={reactlogo} alt="react.svg" width='40px' />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+  )
+}
+
+export default Navbar
